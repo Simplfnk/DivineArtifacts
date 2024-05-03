7c55d3d9ec4c596bdc7192104014cd4eef5007da
Simplfnk <noahkassem@gmail.com>
2024-05-04 02:02:30 +0300

    adding SFX for toggling on/off the abilities adding missing translation key bug fixes

--- src/main/java/nk/divineartifacts/events/PlayerDivineUno.java
+++ src/main/java/nk/divineartifacts/events/PlayerDivineUno.java
@@ -15,18 +15,18 @@ import nk.divineartifacts.init.ModItemGod;
 import nk.divineartifacts.init.SoundRegistry;
 import nk.divineartifacts.utils.Utils;
 
-import static nk.divineartifacts.client.handler.ToggleHelper.*;
+import static nk.divineartifacts.client.handler.ToggleHelper.toggleShield;
 import static nk.divineartifacts.config.ServerConfig.configDivineRing;
 import static nk.divineartifacts.events.DivineHelper.*;
 
-@Mod.EventBusSubscriber( bus = Mod.EventBusSubscriber.Bus.FORGE )
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class PlayerDivineUno {
-	@SubscribeEvent( priority = EventPriority.HIGH )
+	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void onLivingAttack(LivingAttackEvent event) {
-		if ( ! configDivineRing.get() ) return;
-		if (!toggleShield() || configDivineRing.get()) return;
-		if ( event.getEntity() instanceof Player player ) {
-			if ( player.isCreative() || player.isSpectator() ) return;
+		if (!configDivineRing.get()) return;
+		if (!toggleShield()) return;
+		if (event.getEntity() instanceof Player player) {
+			if (player.isCreative() || player.isSpectator()) return;
 			ItemStack ring = Utils.getFirstCurio(ModItemGod.ringDivine.get() , player);
 			DamageSource damageSource = event.getSource();
 			String entName1 = event.getSource().getEntity() != null ? event.getSource().getEntity().getDisplayName().getString() : "";
@@ -36,36 +36,36 @@ public class PlayerDivineUno {
 			boolean NameTrue1 = Names.stream().anyMatch(name -> name.equalsIgnoreCase(entName1));
 			boolean NameTrue2 = Names.stream().anyMatch(name -> name.equalsIgnoreCase(entName2));
 			boolean isFireDamage = FIRE_DAMAGE_TYPES.stream().anyMatch(damageSource::is);
-			if ( ring != null ) {
-				if ( isFireDamage ) {
+			if (ring != null) {
+				if (isFireDamage) {
 					player.clearFire();  //  a method to extinguish the player
 					event.setCanceled(true); // Cancel the event to prevent damage
 				}
-				if ( LiveEntity && ! ( notPlayer ) && ! ( NameTrue1 || NameTrue2 ) ) {
+				if (LiveEntity && !(notPlayer) && !(NameTrue1 || NameTrue2)) {
 					Entity attacker = event.getSource().getEntity();
 					attacker.level().playSound(null , attacker.getX() , attacker.getY() , attacker.getZ() , SoundRegistry.FORCE_SH.get() , SoundSource.PLAYERS , 1.0F , 1.0F);
 					PunishAttackerWithMobEffects(attacker);
 					applyKnockBack(player , attacker);
 					event.setCanceled(true);
 				}
-				if ( NameTrue1 ) {
+				if (NameTrue1) {
 					Entity attacker = event.getSource().getEntity();
 					attacker.level().playSound(null , attacker.getX() , attacker.getY() , attacker.getZ() , SoundRegistry.FORCE_SH.get() , SoundSource.PLAYERS , 1.0F , 1.0F);
 					applyKnockBack(player , attacker);
 					attacker.kill();
 					event.setCanceled(true);
 				}
-				if ( NameTrue2 ) {
+				if (NameTrue2) {
 					Entity sources = event.getSource().getDirectEntity();
 					sources.level().playSound(null , sources.getX() , sources.getY() , sources.getZ() , SoundRegistry.FORCE_SH.get() , SoundSource.PLAYERS , 1.0F , 1.0F);
 					applyKnockBack(player , sources);
 					sources.kill();
 					event.setCanceled(true);
 				}
-				if ( event.getSource().getDirectEntity() instanceof Arrow arrow ) {
+				if (event.getSource().getDirectEntity() instanceof Arrow arrow) {
 					arrow.kill();
 				}
-				else if ( ! ( notPlayer ) ) {
+				else if (!(notPlayer)) {
 					event.setCanceled(true);
 				}
 			}
